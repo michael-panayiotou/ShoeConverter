34cca03161007544a20c05b74ca2cb28252ae71d
michael-panayiotou <michael_panayiotou@icloud.com>
2015-01-01 15:13:16 +0200

    First rev

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoesSizeTextField: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    
+    @IBOutlet weak var womensShowSizeTextField: UITextField!
+    
+    @IBOutlet weak var womansConvertedShoeSizeLabel: UILabel!
+    
+    
+    @IBOutlet weak var pictureImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+//        
+//        let sizeFromTextField = mensShoesSizeTextField.text
+//        let numberFromTextField = sizeFromTextField.toInt()
+//        var integerFromTextField = numberFromTextField!
+        
+        var integerFromTextField = mensShoesSizeTextField.text.toInt()!
+        
+        let conversionConstant = 30
+        
+        integerFromTextField = integerFromTextField + conversionConstant
+        
+        mensConvertedShoeSizeLabel.hidden=false
+        
+        let stringWithUpdatedShowSize = "\(integerFromTextField)"
+        
+        mensConvertedShoeSizeLabel.text=stringWithUpdatedShowSize
+        
+        mensShoesSizeTextField.text=""
+        
+        mensShoesSizeTextField.resignFirstResponder()
+        
+        
+        
+    }
+    
+    
+    @IBAction func convertButtonWomansPressed(sender: UIButton) {
+        
+        
+        let sizeFromTextField = Double((womensShowSizeTextField.text as NSString).doubleValue)
+        
+        let convertsionConstant = 30.5
+        
+        womansConvertedShoeSizeLabel.hidden=false
+        womansConvertedShoeSizeLabel.text = "\(sizeFromTextField + convertsionConstant)"
+    }
 
 }
 
